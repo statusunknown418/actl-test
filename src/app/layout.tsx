ba7998cd916833c8af79e15a2397effdd3cc410a
alvaro <alvarodevcode@outlook.com>
2024-10-14 20:54:21 -0500

    fix: small fixes (not related to challenge)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
